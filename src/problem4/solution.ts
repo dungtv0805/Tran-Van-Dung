5b594fa2b6f5ec870c5af519fd314eb1dc684c1a
chimseo <macbook@chimseo-2.local>
2025-01-17 15:18:36 +0700

    add signum to problem4

--- src/problem4/solution.ts
+++ src/problem4/solution.ts
@@ -6,11 +6,12 @@ function sum_to_n_a(n: number): number {
 
 function sum_to_n_b(n: number): number {
   let res = 0;
+  const signNum = n > 0 ? 1 : -1;
   for (let i = 0; i <= n; i++) res += i;
-  return res;
+  return res * signNum;
 }
 
 function sum_to_n_c(n: number): number {
-  if (n) return n + sum_to_n_c(n - 1);
+  if (n) return n + sum_to_n_c(n - (n > 0 ? 1 : -1));
   return 0;
 }
